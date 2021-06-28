a327970ef5977ee785cd05e03f61fa66bdbed64f
logesh-imf <logeshkumarsuresh@gmail.com>
2021-06-28 16:33:48 +0530

    Background color Animation

---
+++ donate_now/lib/Design.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Design {
+  static const Color backgroundColor = Color(0xFF1565D3);
+}
--- donate_now/lib/login/login_home.dart
+++ donate_now/lib/login/login_home.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:donate_now/Design.dart';
+import 'dart:async';
 
-class Loginhome extends StatelessWidget {
-  // const Loginhome({Key? key}) : super(key: key);
+bool timeLogo = false;
+
+class Loginhome extends StatefulWidget {
+  // const Loginhome({ Key? key }) : super(key: key);
+
+  @override
+  _LoginhomeState createState() => _LoginhomeState();
+}
+
+class _LoginhomeState extends State<Loginhome> {
+  bool bgColor = false;
+  double r = 1;
+
+  void bgColorTimer() {
+    Timer(Duration(seconds: 2), () {
+      setState(() {
+        bgColor = true;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    bgColorTimer();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.blue,
+      body: Stack(
+        children: [
+          Center(
+            child: AnimatedContainer(
+              height: bgColor ? 1000 : 0,
+              width: bgColor ? 1000 : 0,
+              duration: Duration(seconds: 2),
+              color: Design.backgroundColor,
+            ),
+          ),
+          Center(
+            child: Image(
+              image: AssetImage('images/donate_logo.png'),
+              height: 250,
+              width: 250,
+            ),
+          ),
+        ],
       ),
     );
   }
