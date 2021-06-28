45438ac4d4265b433ffcf2b591992e39e9f68d89
logesh-imf <logeshkumarsuresh@gmail.com>
2021-06-28 07:19:23 +0530

    Navigation page added

---
+++ donate_now/lib/login/login_home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Loginhome extends StatelessWidget {
+  // const Loginhome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+}
--- donate_now/lib/main.dart
+++ donate_now/lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:donate_now/login/google_sign_in.dart';
+import 'package:donate_now/login/login_home.dart';
+import 'package:donate_now/homepage.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
@@ -12,10 +21,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Donate Now'),
-          )),
+      home: Navigate(),
     );
   }
+}
+
+class Navigate extends StatelessWidget {
+  // const Navigate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChangeNotifierProvider(
+        create: (context) => GoogleSignInProvider(),
+        child: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            final provider = Provider.of<GoogleSignInProvider>(context);
+
+            if (provider.isSigningIn)
+              return buildLoading();
+            else if (snapshot.hasData)
+              return Homepage();
+            else
+              return Loginhome();
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildLoading() => Stack(
+        fit: StackFit.expand,
+        children: [
+          // CustomPaint(painter: BackgroundPainter()),
+          Center(child: CircularProgressIndicator()),
+        ],
+      );
 }
